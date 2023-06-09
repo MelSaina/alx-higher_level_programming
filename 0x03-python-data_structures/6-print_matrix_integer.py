982755fca04629cb5b30c1cb230a2c297e78ea9f
MelSaina <gabbysiana@gmail.com>
2023-06-09 10:23:19 -0700

    6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """Print a matrix of integers."""
+    for n in range(len(matrix)):
+        for m in range(len(matrix[n])):
+                print("{:d}".format(matrix[n][m]), end="")
+                if m != (len(matrix[n]) - 1):
+                    print(" ", end="")
+
+        print("")
