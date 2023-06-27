b91d6dfa3ce728aac8fb0e23f3314cb227e12e7d
MelSaina <gabbysiana@gmail.com>
2023-06-27 19:53:55 +0300

    main

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Class that defines properties of square
+    
+    Attributes:
+        size: size of a square
+    """
+    def __init__(self, size):
+        
+        """
+        Creates new instances of square
+        Args:
+            size:size of the square
+        """
+        self.__size = size
