fb7f77b0b6888814d4bedb078913bfbf77b409a6
MelSaina <gabbysiana@gmail.com>
2023-06-30 06:50:19 -0700

    mandatory

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,32 @@
+def matrix_divided(matrix, div):
+    """Divides all elements of a matrix.
+    Args:
+        matrix (list of lists): The matrix to be divided.
+        div (number): The number to divide the matrix by.
+    Returns:
+        list of lists: The new matrix, with all elements divided by div.
+    Raises:
+        TypeError: If matrix is not a list of lists of integers/floats.
+        TypeError: If each row of the matrix is not of the same size.
+        TypeError: If div is not a number.
+        ZeroDivisionError: If div is equal to 0.
+    """
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for element in row:
+            new_element = round(element / div, 2)
+            new_row.append(new_element)
+        new_matrix.append(new_row)
+    return new_matrix
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    """Prints a text with 2 new lines after each of these characters: ., ? and :.
+    Args:
+        text (str): The text to be printed.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for line in text.splitlines():
+        print(line.strip())
+        print()
+        print()
