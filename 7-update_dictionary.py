2fb2d05d424c73f435a301dee6c0d4ba24eea0cd
MelSaina <gabbysiana@gmail.com>
2023-07-15 04:13:53 -0700

    changes

--- 7-update_dictionary.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def update_dictionary(a_dictionary, ky, val):
-    a_dictionary[ky] = val
-    return a_dictionary
--- 8-simple_delete.py
+++
@@ -1,4 +0,0 @@
-def simple_delete(a_dictionary, ky=""):
-    if ky in a_dictionary:
-        del a_dictionary[ky]
-    return a_dictionary
