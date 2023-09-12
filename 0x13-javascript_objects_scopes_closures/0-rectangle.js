9a94a08c1e4ed7d78fddc3907ba5198909840cc4
MelSaina <gabbysiana@gmail.com>
2023-09-12 08:02:53 +0000

    the main tasks

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+//Describing a recangle
+
+class Rectangle {
+}
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Defining a class and initializing it
+
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
