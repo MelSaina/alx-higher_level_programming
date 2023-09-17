45a03719859364afa52573ebc8e881433286a78b
MelSaina <gabbysiana@gmail.com>
2023-09-17 01:58:56 -0700

    changes

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,44 +1,24 @@
 #!/usr/bin/python3
+"""
+Script that lists all cities from the database hbtn_0e_4_usa
+"""
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    # Check if the correct number of arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
-        sys.exit(1)
+# The code should not be executed when imported
+if __name__ == '__main__':
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
-    # Extract the command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id ASC")
 
-    try:
-        # Connect to the MySQL database running on localhost at port 3306
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
-
-        # Create a cursor object
-        cursor = db.cursor()
-
-        # Execute the SQL query to fetch cities with their state names
-        cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
-                        JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
-
-        # Fetch all the rows and display them as specified
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
-    finally:
-        # Close the database connection
-        db.close()
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # Clean up process
+    cur.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,46 +1,28 @@
 #!/usr/bin/python3
+"""
+Script that takes in the name of a state as an argument and lists
+all cities of that state, using the database
+"""
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    # Check if the correct number of arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: {} <mysql username> <mysql password> <database name> <state name>".format(sys.argv[0]))
-        sys.exit(1)
+# The code should not be executed when imported
+if __name__ == '__main__':
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
-    # Extract the command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                WHERE states.name = %s", [argv[4]])
 
-    try:
-        # Connect to the MySQL database running on localhost at port 3306
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
+    rows = cur.fetchall()
+    j = []
+    for i in rows:
+        j.append(i[1])
+    print(", ".join(j))
 
-        # Create a cursor object
-        cursor = db.cursor()
-
-        # Use a parameterized query to safely filter cities by state name
-        query = "SELECT GROUP_CONCAT(name ORDER BY cities.id ASC) FROM cities \
-                 JOIN states ON cities.state_id = states.id WHERE states.name=%s"
-        cursor.execute(query, (state_name,))
-
-        # Fetch the result and display it
-        result = cursor.fetchone()
-        if result and result[0]:
-            print(result[0])
-
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
-    finally:
-        # Close the database connection
-        db.close()
+    # Clean up process
+    cur.close()
+    db.close()
