e8bdf75a467b15936b2cb55336cf79f3e767e619
MelSaina <gabbysiana@gmail.com>
2023-09-17 01:41:28 -0700

    2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,45 +1,30 @@
 #!/usr/bin/python3
-import sys
+"""
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument
+"""
 import MySQLdb
+from sys import argv
 
-if __name__ == "__main__":
-    # Check if the correct number of arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: {} <mysql username> <mysql password> <database name> <state name>".format(sys.argv[0]))
-        sys.exit(1)
+if __name__ == '__main__':
+    # Extract command-line arguments
+    username, password, database, state_name = argv[1], argv[2], argv[3], argv[4]
 
-    # Extract the command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+    # Connect to the MySQL database running on localhost at port 3306
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
 
-    try:
-        # Connect to the MySQL database running on localhost at port 3306
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
+    # Create a cursor object
+    cur = db.cursor()
 
-        # Create a cursor object
-        cursor = db.cursor()
+    # Create and execute the SQL query
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (state_name,))
 
-        # Use parameterized query to avoid SQL injection
-        query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
-        cursor.execute(query, (state_name,))
+    # Fetch all the rows and display them as specified
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-        # Fetch all the rows and display them as they are in the example
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
-    finally:
-        # Close the database connection
-        db.close()
+    # Clean up
+    cur.close()
+    db.close()
