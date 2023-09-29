78968b3530646b1574a571e4331d1ea89ffe1fd8
MelSaina <gabbysiana@gmail.com>
2023-09-29 09:20:11 -0700

    3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,19 +6,17 @@ body of the response (decoded in utf-8).
 Usage: ./3-error_code.py <URL>
   - Handles HTTP errors.
 """
-import urllib.request
-import urllib.error
 import sys
+import urllib.error
+import urllib.request
 
-# Extract the URL from the command line argument
-url = sys.argv[1]
 
-try:
-    # Send a GET request to the URL
-    with urllib.request.urlopen(url) as response:
-        # Read and decode the response body as UTF-8
-        body = response.read().decode('utf-8')
-        print(body)
-except urllib.error.HTTPError as e:
-    # Handle HTTP errors by printing the error code
-    print("Error code:", e.code)
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
