4ce34124a7e5baf03cfd146fdb7f330872e0c4c6
MelSaina <gabbysiana@gmail.com>
2023-09-29 07:31:09 -0700

    0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+try:
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode('utf-8'))
+except Exception as e:
+    print(e)
