68038afd45288b6632d2ec90b4888c535a446089
MelSaina <gabbysiana@gmail.com>
2023-09-29 07:38:39 -0700

    1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response.
+
+Usage: ./1-hbtn_header.py <URL>
+"""
+import urllib.request
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: ./1-hbtn_header.py <URL>")
+    sys.exit(1)
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        headers = response.info()
+        x_request_id = headers.get('X-Request-Id')
+        if x_request_id:
+            print(x_request_id)
+except Exception as e:
+    print(e)
+    sys.exit(1)
